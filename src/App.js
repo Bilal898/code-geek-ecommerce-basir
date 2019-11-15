46f783c7cc987ed8bd12a3951e197e46b4b120ac
bilal898 <bfhamid@gmail.com>
2019-11-15 20:03:12 +0500

    inital commit with filters working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+import React, { Component } from 'react'
+import Products from './components/Products'
+import Filter from './components/Filter';
+export default class App extends Component {
+
+  state = {
+    products: [],
+    filteredProducts: [],
+    sort: ''
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:8000/products")
+    .then(res => res.json())
+    . then(data => this.setState({
+        products: data,
+        filteredProducts: data
+    }))
+  }
+
+  handleChangeSort = (e) => {
+    this.setState({
+      sort: e.target.value
+    })
+    this.listProducts()
+  }
+
+  listProducts = () => {
+    this.setState( state => {
+      if(state.sort !== ''){
+        state.products.sort((a,b) => (state.sort === 'lowest' 
+        ? (a.price > b.price ? 1: -1)
+        : (a.price < b.price ? 1: -1)
+        ))
+      } else {
+        state.products.sort((a,b) => (a.id < b.di ?1: -1))
+      }
+    }) 
+  }
+  render() {
+    return (
+      <div className="container">
+      <h1>Ecommerce Shopping Cart App</h1>
+      <hr />
+      <div className="row"> 
+        <div className="col-md-8">
+        <Filter size={this.state.size} 
+          sort={this.state.sort}
+          handleChangeSize={this.handleChangeSize}
+          handleChangeSort={this.handleChangeSort}
+          count={this.state.filteredProducts.length}
+        />
+        <hr/>
+          <Products 
+            products={this.state.filteredProducts} 
+            handleAddToCart={this.handleAddToCart} 
+            />
+        </div>
+      </div>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
